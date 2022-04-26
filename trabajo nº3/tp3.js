b023efbd36efcc5484373e5f73731def9d231e8d
agustina0203 <103397134+agustina0203@users.noreply.github.com>
2022-04-26 04:12:15 -0300

    Add files via upload

---
+++ trabajo nº3/tp3.js
@@ -0,0 +1,9 @@
+var principal = document.getElementById('agustina')
+var etiquetah1 = document.createElement('h1')
+
+//verificacion del codigo//
+console.log(h1);
+
+//informacion de la etiqueta//
+etiquetah1.innerHTML = "tp3"
+agustina.appendChild(etiquetah1)
